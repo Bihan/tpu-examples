cca32e04a05a05aa0c8fb58c1afbada3048705bd
Bihan  Rana <bihan@Bihans-MacBook-Pro.local>
2025-01-30 14:12:05 +0545

    Add GPU check

--- gaudi/sft_train.py
+++ gaudi/sft_train.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
 from trl import SFTConfig, SFTTrainer
+import torch
 
 dataset = load_dataset("stanfordnlp/imdb", split="train")
 
@@ -12,4 +13,9 @@ trainer = SFTTrainer(
     train_dataset=dataset,
     args=training_args,
 )
+# 4. Print relevant info
+print("Is GPU available? ", torch.cuda.is_available())
+print("Trainer device:", trainer.args.device)
+print("Model device:", trainer.model.device)
+
 trainer.train()
