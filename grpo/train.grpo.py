dffc1124dcdcad0068d5bfeaa0e44d28fa3fd74d
Bihan  Rana <bihan@Bihans-MacBook-Pro.local>
2025-02-06 13:46:05 +0545

    Add Deepseek Qwen

--- grpo/train.grpo.py
+++ grpo/train.grpo.py
@@ -9,8 +9,8 @@ dataset = load_dataset("trl-lib/tldr", split="test")
 def reward_len(completions, **kwargs):
     return [abs(20 - len(completion)) for completion in completions]
 
-training_args = GRPOConfig(output_dir="Deepseek-V2-GRPO", logging_steps=10, per_device_train_batch_size=1)
-model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-V2-Lite", trust_remote_code=True)
+training_args = GRPOConfig(output_dir="Deepseek-Distill-Qwen-1.5B-GRPO", logging_steps=10, per_device_train_batch_size=1)
+model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", trust_remote_code=True)
 trainer = GRPOTrainer(
     model=model,
     reward_funcs=reward_len,
