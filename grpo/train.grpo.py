188ad48b742f7e64ddffbdd4bd5a0bed82606016
Bihan  Rana <bihan@Bihans-MacBook-Pro.local>
2025-02-06 13:18:01 +0545

    Add Deepseek V2

--- grpo/train.grpo.py
+++ grpo/train.grpo.py
@@ -1,6 +1,7 @@
 # train_grpo.py
 from datasets import load_dataset
 from trl import GRPOConfig, GRPOTrainer
+from transformers import AutoModelForCausalLM
 
 dataset = load_dataset("trl-lib/tldr", split="test")
 
@@ -8,9 +9,10 @@ dataset = load_dataset("trl-lib/tldr", split="test")
 def reward_len(completions, **kwargs):
     return [abs(20 - len(completion)) for completion in completions]
 
-training_args = GRPOConfig(output_dir="Qwen2-0.5B-GRPO", logging_steps=10, per_device_train_batch_size=1, bf16=True)
+training_args = GRPOConfig(output_dir="Deepseek-V2-GRPO", logging_steps=10, per_device_train_batch_size=1, bf16=True)
+model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-V2-Lite", trust_remote_code=True)
 trainer = GRPOTrainer(
-    model="Qwen/Qwen2-0.5B-Instruct",
+    model=model,
     reward_funcs=reward_len,
     args=training_args,
     train_dataset=dataset
